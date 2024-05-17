77768f02ad32092df324859a0cf28f4fe5b2a1d9
Ankit <ankitsharma.as420@gmail.com>
2024-05-17 20:30:39 +0530

    add new variable in update book details method.

--- OnlineBookstore/src/main/java/com/book/OnlineBookstore/Service/BookService.java
+++ OnlineBookstore/src/main/java/com/book/OnlineBookstore/Service/BookService.java
@@ -9,9 +9,9 @@ import com.book.OnlineBookstore.Entity.Book;
 import com.book.OnlineBookstore.Model.AddBook;
 
 public interface BookService {
-    public String addBook(Book addBook, MultipartFile imagFile) throws IOException;
-    public List<AddBook> getBooks();
+    public String addBook(Book addBook, MultipartFile imageFile) throws IOException;
+    public List<AddBook> getAllBooks();
     public List<AddBook> getBookById(Long id);
-    public String updateBookDetails(Long id, AddBook addBook);
+    public String updateBookDetails(Long id, AddBook addBook, MultipartFile imageFile);
     public Boolean deleteBook(Long id);
 }
